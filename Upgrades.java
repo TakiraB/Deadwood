3362e19eaeaa77dc51db597069131b0adb9a23e7
Takira <123585167+TakiraB@users.noreply.github.com>
2024-05-15 18:11:25 -0700

    created Upgrades class for modularity/organization

---
+++ Upgrades.java
@@ -0,0 +1,35 @@
+public class Upgrades {
+    private int upgradeLevel;
+    private String currencyType;
+    private int upgradeAmount;
+
+    public Upgrades(int upgradeLevel, String currencyType, int upgradeAmount) {
+        this.upgradeLevel=upgradeLevel;
+        this.currencyType = currencyType;
+        this.upgradeAmount = upgradeAmount;
+    }
+
+    public int getUpgradeLevel(){
+        return upgradeLevel;
+    }
+
+    public void setUpgradeLevel(int upgradeLevel){
+        this.upgradeLevel=upgradeLevel;
+    }
+
+    public String getCurrencyType(){
+        return currencyType;
+    }
+
+    public void setCurrencyType(String currencyType){
+        this.currencyType=currencyType;
+    }
+
+    public int getUpgradeAmount(){
+        return upgradeAmount;
+    }
+
+    public void setUpgradeAmount(int upgradeAmount){
+        this.upgradeAmount=upgradeAmount;
+    }
+}
