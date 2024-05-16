bb70bff85434f7722de2d82cbe567d56fc648646
Takira <123585167+TakiraB@users.noreply.github.com>
2024-05-15 17:12:33 -0700

    creating Takes class for scene takes

---
+++ Takes.java
@@ -0,0 +1,16 @@
+public class Takes {
+    private int takesForSuccess;
+
+
+    public Takes(int takesForSuccess){
+        this.takesForSuccess=takesForSuccess;
+    }
+
+    public void setTake(int takesForSuccess){
+        this.takesForSuccess=takesForSuccess;
+    }
+
+    public int getTake(){
+        return takesForSuccess;
+    }
+}
