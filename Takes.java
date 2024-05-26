2b1635d2c67f2cb4fb4ece58e7c13e57b5080cab
Takira <123585167+TakiraB@users.noreply.github.com>
2024-05-25 18:39:21 -0700

    added takes area

--- Takes.java
+++ Takes.java
@@ -1,16 +1,24 @@
 public class Takes {
     private int takesForSuccess;
+    private Area takesArea;
 
-
-    public Takes(int takesForSuccess){
-        this.takesForSuccess=takesForSuccess;
+    public Takes(int takesForSuccess) {
+        this.takesForSuccess = takesForSuccess;
     }
 
-    public void setTake(int takesForSuccess){
-        this.takesForSuccess=takesForSuccess;
+    public void setTake(int takesForSuccess) {
+        this.takesForSuccess = takesForSuccess;
     }
 
-    public int getTake(){
+    public int getTake() {
         return takesForSuccess;
+    }
+
+    public Area getTakeArea(){
+        return takesArea;
+    }
+
+    public void setTakeArea(Area takesArea){
+        this.takesArea = takesArea;
     }
 }
