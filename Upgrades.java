6a2951afbe5b88f69d51dcb280613233044f2982
Takira <123585167+TakiraB@users.noreply.github.com>
2024-05-25 18:40:01 -0700

    added upgrade choice areas

--- Upgrades.java
+++ Upgrades.java
@@ -2,35 +2,44 @@ public class Upgrades {
     private int upgradeLevel;
     private String currencyType;
     private int upgradeAmount;
+    private Area choiceArea;
 
     public Upgrades(int upgradeLevel, String currencyType, int upgradeAmount) {
-        this.upgradeLevel=upgradeLevel;
+        this.upgradeLevel = upgradeLevel;
         this.currencyType = currencyType;
         this.upgradeAmount = upgradeAmount;
     }
 
-    public int getUpgradeLevel(){
+    public int getUpgradeLevel() {
         return upgradeLevel;
     }
 
-    public void setUpgradeLevel(int upgradeLevel){
-        this.upgradeLevel=upgradeLevel;
+    public void setUpgradeLevel(int upgradeLevel) {
+        this.upgradeLevel = upgradeLevel;
     }
 
-    public String getCurrencyType(){
+    public String getCurrencyType() {
         return currencyType;
     }
 
-    public void setCurrencyType(String currencyType){
-        this.currencyType=currencyType;
+    public void setCurrencyType(String currencyType) {
+        this.currencyType = currencyType;
     }
 
-    public int getUpgradeAmount(){
+    public int getUpgradeAmount() {
         return upgradeAmount;
     }
 
-    public void setUpgradeAmount(int upgradeAmount){
-        this.upgradeAmount=upgradeAmount;
+    public void setUpgradeAmount(int upgradeAmount) {
+        this.upgradeAmount = upgradeAmount;
+    }
+
+    public Area getUpgradeArea(){
+        return choiceArea;
+    }
+
+    public void setUpgradeArea(Area choiceArea){
+        this.choiceArea = choiceArea;
     }
 
 }
