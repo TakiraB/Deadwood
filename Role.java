6edbd430003f51a27852d031c708e7f06d2715a9
Takira <123585167+TakiraB@users.noreply.github.com>
2024-05-25 18:38:04 -0700

    added role area

--- Role.java
+++ Role.java
@@ -4,17 +4,18 @@ public class Role {
     private boolean starredRole;
     private int rank;
     private Player playerOnRole;
+    private Area roleArea;
 
-    public Role(String name, String script, boolean starredRole, int rank){
+    public Role(String name, String script, boolean starredRole, int rank) {
         this.name = name;
         this.script = script;
-        // this.isActive = isActive;
         this.starredRole = starredRole;
         this.rank = rank;
+        this.playerOnRole = null;
     }
 
     // check if the tole is active, like if it is being rehearsed or acted on
-    public boolean checkActiveRole(Player player){
+    public boolean checkActiveRole(Player player) {
         return true;
     }
 
@@ -33,53 +34,60 @@ public class Role {
 
     // getters and setters
 
-    public String getRoleName(){
+    public String getRoleName() {
         return name;
     }
 
-    public String getScript(){
+    public String getScript() {
         return script;
     }
 
-    public int getRank(){
+    public int getRank() {
         return rank;
     }
 
     // public boolean getActive(){
-    //     return isActive;
+    // return isActive;
     // }
 
-    public boolean getStarredRole(){
+    public boolean getStarredRole() {
         return starredRole;
     }
 
-    public void setRoleName(String name){
+    public void setRoleName(String name) {
         this.name = name;
     }
 
-    public void setRoleScript(String script){
+    public void setRoleScript(String script) {
         this.script = script;
     }
 
     // public void setActiveRole(boolean isActive){
-    //     this.isActive = isActive;
+    // this.isActive = isActive;
     // }
 
-    public void setStarredRole(boolean starredRole){
+    public void setStarredRole(boolean starredRole) {
         this.starredRole = starredRole;
     }
 
-    public void setRank(int rank){
+    public void setRank(int rank) {
         this.rank = rank;
     }
 
-    public Player getPlayerOnRole(){
+    public Player getPlayerOnRole() {
         return playerOnRole;
     }
 
-    public void setPlayer(Player newPlayer){
+    public void setPlayerOnRole(Player newPlayer) {
         this.playerOnRole = newPlayer;
     }
 
-    
+    public Area getRoleArea(){
+        return roleArea;
+    }
+
+    public void setRoleArea(Area roleArea) {
+        this.roleArea = roleArea;
+    }
+
 }
