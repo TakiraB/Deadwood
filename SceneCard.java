ecaafebcbac3cf75b1d93a4c9b8be8a3c0724fa1
Takira <123585167+TakiraB@users.noreply.github.com>
2024-05-28 15:57:27 -0700

    added area object for scene cards

--- SceneCard.java
+++ SceneCard.java
@@ -8,8 +8,10 @@ public class SceneCard {
     private int sceneNumber;
     private String description;
     private String imagePlaceholder;
+    private Area onCardArea;
 
-    public SceneCard(String name, int budget, List<Role> roles, int sceneNumber, String description, String imagePlaceholder) {
+    public SceneCard(String name, int budget, List<Role> roles, int sceneNumber, String description,
+            String imagePlaceholder) {
         this.name = name;
         this.budget = budget;
         this.roles = roles;
@@ -20,11 +22,10 @@ public class SceneCard {
 
     // // TODO: get the information for the SceneCard
     // public SceneCard getSceneCard() {
-    //     return this;
+    // return this;
     // }
 
-    // Getters and setters 
-
+    // Getters and setters
 
     public String getName() {
         return name;
@@ -58,20 +59,28 @@ public class SceneCard {
         this.description = description;
     }
 
-    public void setSceneNumber(int sceneNumber){
-        this.sceneNumber=sceneNumber;
+    public void setSceneNumber(int sceneNumber) {
+        this.sceneNumber = sceneNumber;
     }
 
-    public int getSceneNumber(){
+    public int getSceneNumber() {
         return sceneNumber;
     }
 
-    public void setImage (String imagePlaceholder){
-        this.imagePlaceholder=imagePlaceholder;
+    public void setImage(String imagePlaceholder) {
+        this.imagePlaceholder = imagePlaceholder;
     }
 
-    public String getImage(){
+    public String getImage() {
         return imagePlaceholder;
+    }
+
+    public Area getSceneCardArea() {
+        return onCardArea;
+    }
+
+    public void setSceneCardArea(Area onCardArea){
+        this.onCardArea = onCardArea;
     }
 
 }
