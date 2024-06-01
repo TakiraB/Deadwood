d73686a54ccee9aff54d91f02d7e840f9ac2d22c
Takira <123585167+TakiraB@users.noreply.github.com>
2024-06-01 13:18:45 -0700

    Update DeadwoodController.java

--- DeadwoodController.java
+++ DeadwoodController.java
@@ -19,14 +19,47 @@ public class DeadwoodController {
 
     }
 
-    // initialize game class (maybe include initialization of players too? or just the scene cards)
+    // set up the board
+    public void setUpBoard() {
+        Document doc = null;
+        parseBoard parsingBoard = new parseBoard();
+        Board board = new Board();
+        try {
+           doc = parsingBoard.getDocFromFile("board.xml");
+           board = parsingBoard.readBoardData(doc);
+        } catch (Exception e) {
+           System.out.println("Error = " + e);
+        }
+    }
 
-    // setPlayersClass (may get absorbed into method above, or set the player count)
+    // setup the scene cards for each room
+    public void setUpSceneCards() {
+        Document doc = null;
+        parseCards parsingCards = new parseCards();
+        ArrayList<SceneCard> cards = new ArrayList<SceneCard>();
+        try {
+            doc = parsingCards.getDocFromFile("cards.xml");
+            cards = parsingCards.readCardData(doc);
+        } catch (Exception e) {
+            System.out.println("Error = " + e);
+        }
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < cards.size(); i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+
+        // giving each RoomWithScene a sceneCard
+        board.sceneCardDistribution(numbers, cards, 1);
+
+        GamePieceManager gamePieceManager = new GamePieceManager(6, cards);
+    }
+
+    // Create Player objects based on the number of players
     public void setActivePlayers(int playerCount){
     // if (playerCount < 2 || playerCount > 8) {
     //     JOptionPane.showMessageDialog(view,"That's not an appropriate amount of players! Sorry! :(");
     // }
-    
     for (int i = 0; i < playerCount; i++) {
         String playerName = JOptionPane.showInputDialog("What is the name for Player " + (i + 1) + "?");
         // Initialize Player object for if statement
@@ -44,31 +77,63 @@ public class DeadwoodController {
     // Add player to list of active players
     playerList.add(tempPlayer);
     }
-
-    // creation of GameState with the first activePlayer being randomized
-    Random random = new Random();
-    int firstPlayerInt = random.nextInt(playerCount);
-    // Initialize the GameState
-    GameState gameState;
-
-    // Setting up the max number of days in the game based on the number of players
-    // 3 if 2 players, and 4 for everything else
-    if (playerCount < 3) {
-        gameState = new GameState(playerList.get(firstPlayerInt), playerList, 1, 3, board);
-    } else {
-        gameState = new GameState(playerList.get(firstPlayerInt), playerList, 1, 4, board);
-    }
-    
-    // Start the game
-    gameState.startGame();
+    view.setPlayerIcons(playerCount);
 }
 
+    // // creation of GameState with the first activePlayer being randomized
+    // Random random = new Random();
+    // int firstPlayerInt = random.nextInt(playerCount);
+    // // Initialize the GameState
+    // GameState gameState;
 
-    // act class
+    // // Setting up the max number of days in the game based on the number of players
+    // // 3 if 2 players, and 4 for everything else
+    // if (playerCount < 3) {
+    //     gameState = new GameState(playerList.get(firstPlayerInt), playerList, 1, 3, board);
+    // } else {
+    //     gameState = new GameState(playerList.get(firstPlayerInt), playerList, 1, 4, board);
+    // }
+    
+    // // Start the game
+    // gameState.startGame();
 
-    // rehearse class
 
-    // Listener stuff
+    // initialize the game state
+    public void setGameState(int numPlayers){
+        Random random = new Random();
+        int firstPlayerInt = random.nextInt(numPlayers);
+
+        // Setting up the max number of days in the game based on the number of players
+        // 3 if 2 players, and 4 for everything else
+        if (numPlayers < 3) {
+            gameState = new GameState(playerList.get(firstPlayerInt), playerList, 1, 3, board);
+        } else {
+            gameState = new GameState(playerList.get(firstPlayerInt), playerList, 1, 4, board);
+        }     
+    }
+
+    // initialize the active players in the game
+    public void initializeActivePlayers(int numPlayers){
+        setGameState(numPlayers);
+        setActivePlayers(numPlayers);
+    }
+
+    // move action listener
+
+    // act action listener
+
+    // rehearse action listener
+
+    // upgrade action listener
+
+    // taking role action listener
+
+    // displaying stats action listener
+
+    // display players action listener
+
+    // need some kind of start game method
+
 }
 
 
